050bd2f22b0e9f37957bb8b8f3f1db7a23e1c351
Rein Undheim <rein@undheim.io>
2024-11-23 18:29:48 +0100

    chore(dmn): correct jsr

--- packages/dmn-differ/src/index.ts
+++ packages/dmn-differ/src/index.ts
@@ -1,5 +1,5 @@
 export { Differ } from "./differ";
 export { diff } from "./diff";
-export { default as camundaScheme } from "../resources/camunda.json" assert {
+export { default as camundaScheme } from "../resources/camunda.json" with {
 	type: "json",
 };
